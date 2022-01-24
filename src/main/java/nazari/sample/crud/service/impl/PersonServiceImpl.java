73345d07db8ef55b45fb5b1a931f4e9b9d06698d
mahmood <mahmood.nazari93@gmail.com>
2022-01-24 15:01:27 +0330

    add validation for person save and add @Transactional above the person service methods

--- src/main/java/nazari/sample/crud/service/impl/PersonServiceImpl.java
+++ src/main/java/nazari/sample/crud/service/impl/PersonServiceImpl.java
@@ -6,6 +6,7 @@ import nazari.sample.crud.model.dto.PersonDTO;
 import nazari.sample.crud.repository.IPersonDao;
 import nazari.sample.crud.service.IPersonService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,29 +20,38 @@ public class PersonServiceImpl implements IPersonService {
     }
 
     @Override
+    @Transactional
     public Person saveOrUpdate(PersonDTO personDTO) {
         Person person = IPersonMapper.INSTANCE.personDTOToPerson(personDTO);
         /*
          * when try to save a person without car, because of mapping null carId map to car.id and it will save to Database with null values except id
          * this line make car object null to prevent save it
          * */
-        person.setCar(null);
+        validationForPreventSaveCarBecauseMapping(person);
         return iPersonDao.save(person);
     }
 
+    private void validationForPreventSaveCarBecauseMapping(Person person) {
+        if (person.getCar().getId() == null)
+            person.setCar(null);
+    }
+
     @Override
+    @Transactional
     public PersonDTO getById(Long id) {
         Person person = iPersonDao.findById(id).orElse(null);
         return IPersonMapper.INSTANCE.personToPersonDTO(person);
     }
 
     @Override
+    @Transactional
     public List<PersonDTO> getAllPerson() {
         List<Person> all = iPersonDao.findAll();
         return IPersonMapper.INSTANCE.personToPersonDTO(all);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         iPersonDao.deleteById(id);
     }
